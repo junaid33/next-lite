fc32099d8e8f3efb8ab95f3dee4629e1613cf2ab
Junaid <junaidkabani3@gmail.com>
2021-05-01 17:09:46 -0500

    Try pages folder again

--- keystone.ts
+++ keystone.ts
@@ -72,10 +72,38 @@ export default withAuth(
             mode: "write",
             outputPath: "pages/welcome.js",
             src: `
-              import { jsx } from '@keystone-ui/core';
-              export default function Welcome() {
-                return (<h1>Welcome to my keystone system</h1>);
-              }`,
+            import React from 'react';
+            import Image from 'next/image';
+            import Link from 'next/link';
+            import { getCurrentUser } from '../requestAuth';
+            // eslint-disable-next-line import/no-unresolved
+            import { lists } from '.keystone/api';
+            
+            export default function HomePage({ posts, authedUser }) {
+              return (
+                <div>
+                  <Image src="/logo.svg" width="38" height="38" alt="Keystone" />
+                  <h1 style={{ display: 'inline', marginLeft: '10px', verticalAlign: 'top' }}>
+                    Welcome to my blog
+                  </h1>
+                  <ul>
+                    {posts.map((post, i) => (
+                      <li key={i}>
+                        
+                          <a>{post.title}</a>
+                        
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              );
+            }
+            
+            export async function getServerSideProps({ req }) {
+              const authedUser = await getCurrentUser(req);
+              const posts = await lists.Post.findMany({ query: 'slug title' });
+              return { props: { posts, authedUser } };
+            }`,
           },
         ],
       ],
---
+++ pages/juju/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { getCurrentUser } from '../../requestAuth';
+// eslint-disable-next-line import/no-unresolved
+import { lists } from '.keystone/api';
+
+export default function HomePage({ posts, authedUser }) {
+  return (
+    <div>
+      <Image src="/logo.svg" width="38" height="38" alt="Keystone" />
+      <h1
+        style={{ display: 'inline', marginLeft: '10px', verticalAlign: 'top' }}
+      >
+        Welcome to my blog{authedUser && `, ${authedUser.name}`}
+      </h1>
+      <ul>
+        {posts.map((post, i) => (
+          <li key={i}>
+            <Link href={`/post/${post.slug}`}>
+              <a>{post.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export async function getServerSideProps({ req }) {
+  const authedUser = await getCurrentUser(req);
+  const posts = await lists.Post.findMany({ query: 'slug title' });
+  return { props: { posts, authedUser } };
+}
